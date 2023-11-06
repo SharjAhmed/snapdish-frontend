f90a68070f1a26430e731e16cbe764618f58ea52
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-11-06 23:21:17 +0000

    Set up routing using react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() =><p>Uh Oh! Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
