4c8aa545c8be3768eb02f7302845183af138487a
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-11-09 17:17:04 +0000

    Added SignInForm.js with validation

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
-import styles from "../../styles/Form.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import {
@@ -14,24 +13,21 @@ import {
     Alert,
 } from "react-bootstrap";
 
-
-
-const SignUpForm = () => {
-    const [signUpData, setSignUpData] = useState({
+function SignInForm() {
+    const [signInData, setSignInData] = useState({
         username: "",
-        password1: "",
-        password2: "",
+        password: "",
     });
 
-    const { username, password1, password2 } = signUpData;
+    const { username, password } = signInData;
 
     const [errors, setErrors] = useState({});
 
     const history = useHistory();
 
     const handleChange = (event) => {
-        setSignUpData({
-            ...signUpData,
+        setSignInData({
+            ...signInData,
             [event.target.name]: event.target.value,
         });
     };
@@ -39,8 +35,8 @@ const SignUpForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post("/dj-rest-auth/registration/", signUpData);
-            history.push("/signin");
+            await axios.post("/dj-rest-auth/login/", signInData);
+            history.push("/");
         } catch (err) {
             setErrors(err.response?.data);
         }
@@ -50,7 +46,7 @@ const SignUpForm = () => {
         <Container className="bg-white col-md-8">
             <Row className="p-4 mt-5 justify-content-md-center">
                 <Col className="text-center">
-                    <h1 className="mb-3">Sign Up to SnapDish!</h1>
+                    <h1 className="mb-3">Sign In to SnapDish!</h1>
                     <Row className="justify-content-center">
                         <Form onSubmit={handleSubmit}>
                             <Form.Group controlId="username" className="text-left mb-2">
@@ -69,72 +65,35 @@ const SignUpForm = () => {
                                 </Alert>
                             ))}
 
-                            <Form.Group controlId="password1" className="mb-2">
+                            <Form.Group controlId="password" className="mb-2">
                                 <Form.Label className="d-none">Password</Form.Label>
                                 <Form.Control
                                     type="password"
                                     placeholder="Password"
-                                    name="password1"
-                                    value={password1}
+                                    name="password"
+                                    value={password}
                                     onChange={handleChange}
                                 />
                             </Form.Group>
-                            {errors.password1?.map((message, idx) => (
+                            {errors.password?.map((message, idx) => (
                                 <Alert variant="warning" key={idx}>
                                     {message}
                                 </Alert>
                             ))}
-
-                            <Form.Group controlId="password2">
-                                <Form.Label className="d-none">Confirm Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Confirm password"
-                                    name="password2"
-                                    value={password2}
-                                    onChange={handleChange}
-                                />
-                            </Form.Group>
-                            {errors.password2?.map((message, idx) => (
-                                <Alert variant="warning" key={idx}>
-                                    {message}
-                                </Alert>
-                            ))}
-
-                            <br />
                             <Button
                                 className={btnStyles.Button}
-                                aria-label="Click here to sign up!"
+                                aria-label="Click here to sign in!"
                                 variant="primary"
                                 type="submit"
                             >
-                                Sign Up
+                                Sign In
                             </Button>
-                            {errors.non_field_errors?.map((message, idx) => (
-                                <Alert key={idx} variant="warning" className="mt-3">
-                                    {message}
-                                </Alert>
-                            ))}
                         </Form>
-                    </Row>
-                    <Row className="mt-4 justify-content-center">
-                        <p>
-                            Already have an account?{" "}
-                            <span>
-                                <Link
-                                    className={styles.Link}
-                                    aria-label="Click here to log in instead"
-                                    to="/signin"
-                                >
-                                    Sign In instead!
-                                </Link>
-                            </span>
-                        </p>
                     </Row>
                 </Col>
             </Row>
         </Container>
-    );
-};
+    )
+}
 
-export default SignUpForm;
+export default SignInForm;
